f05de1dafce6b5d851b658233971e2cb5442e308
marcusp619 <marcusp619@gmail.com>
2018-10-24 14:08:25 -0600

    Add utils folder with mockmovie data

---
+++ src/Utils/MockMovies.js
@@ -0,0 +1,413 @@
+export default = {
+    "results": [
+        {
+            "vote_count": 1515,
+            "id": 335983,
+            "video": false,
+            "vote_average": 6.6,
+            "title": "Venom",
+            "popularity": 401.758,
+            "poster_path": "/2uNW4WbgBXL25BAbXGLnLqX71Sw.jpg",
+            "original_language": "en",
+            "original_title": "Venom",
+            "genre_ids": [
+                878,
+                28,
+                80,
+                28,
+                27
+            ],
+            "backdrop_path": "/VuukZLgaCrho2Ar8Scl9HtV3yD.jpg",
+            "adult": false,
+            "overview": "When Eddie Brock acquires the powers of a symbiote, he will have to release his alter-ego \"Venom\" to save his life.",
+            "release_date": "2018-10-03"
+        },
+        {
+            "vote_count": 934,
+            "id": 332562,
+            "video": false,
+            "vote_average": 7.5,
+            "title": "A Star Is Born",
+            "popularity": 196.325,
+            "poster_path": "/wrFpXMNBRj2PBiN4Z5kix51XaIZ.jpg",
+            "original_language": "en",
+            "original_title": "A Star Is Born",
+            "genre_ids": [
+                18,
+                10402,
+                10749
+            ],
+            "backdrop_path": "/840rbblaLc4SVxm8gF3DNdJ0YAE.jpg",
+            "adult": false,
+            "overview": "Seasoned musician Jackson Maine discovers—and falls in love with—struggling artist Ally. She has just about given up on her dream to make it big as a singer—until Jack coaxes her into the spotlight. But even as Ally's career takes off, the personal side of their relationship is breaking down, as Jack fights an ongoing battle with his own internal demons.",
+            "release_date": "2018-10-03"
+        },
+        {
+            "vote_count": 639,
+            "id": 346910,
+            "video": false,
+            "vote_average": 5.3,
+            "title": "The Predator",
+            "popularity": 167.001,
+            "poster_path": "/wMq9kQXTeQCHUZOG4fAe5cAxyUA.jpg",
+            "original_language": "en",
+            "original_title": "The Predator",
+            "genre_ids": [
+                27,
+                878,
+                28,
+                53
+            ],
+            "backdrop_path": "/f4E0ocYeToEuXvczZv6QArrMDJ.jpg",
+            "adult": false,
+            "overview": "From the outer reaches of space to the small-town streets of suburbia, the hunt comes home. Now, the universe’s most lethal hunters are stronger, smarter and deadlier than ever before, having genetically upgraded themselves with DNA from other species. When a young boy accidentally triggers their return to Earth, only a ragtag crew of ex-soldiers and a disgruntled science teacher can prevent the end of the human race.",
+            "release_date": "2018-09-13"
+        },
+        {
+            "vote_count": 147,
+            "id": 424139,
+            "video": false,
+            "vote_average": 6.6,
+            "title": "Halloween",
+            "popularity": 131.422,
+            "poster_path": "/lNkDYKmrVem1J0aAfCnQlJOCKnT.jpg",
+            "original_language": "en",
+            "original_title": "Halloween",
+            "genre_ids": [
+                27
+            ],
+            "backdrop_path": "/hO1oTBGNxO5fBKVEuWnSpICJH7c.jpg",
+            "adult": false,
+            "overview": "Laurie Strode comes to her final confrontation with Michael Myers, the masked figure who has haunted her since she narrowly escaped his killing spree on Halloween night four decades ago.",
+            "release_date": "2018-10-18"
+        },
+        {
+            "vote_count": 273,
+            "id": 369972,
+            "video": false,
+            "vote_average": 7.3,
+            "title": "First Man",
+            "popularity": 123.269,
+            "poster_path": "/i91mfvFcPPlaegcbOyjGgiWfZzh.jpg",
+            "original_language": "en",
+            "original_title": "First Man",
+            "genre_ids": [
+                36,
+                18
+            ],
+            "backdrop_path": "/z6ZANhPCcPl1HsKGjY5iZALf65V.jpg",
+            "adult": false,
+            "overview": "A look at the life of the astronaut, Neil Armstrong, and the legendary space mission that led him to become the first man to walk on the Moon on July 20, 1969.",
+            "release_date": "2018-10-11"
+        },
+        {
+            "vote_count": 1177,
+            "id": 439079,
+            "video": false,
+            "vote_average": 5.8,
+            "title": "The Nun",
+            "popularity": 108.868,
+            "poster_path": "/sFC1ElvoKGdHJIWRpNB3xWJ9lJA.jpg",
+            "original_language": "en",
+            "original_title": "The Nun",
+            "genre_ids": [
+                27,
+                9648,
+                53
+            ],
+            "backdrop_path": "/fgsHxz21B27hOOqQBiw9L6yWcM7.jpg",
+            "adult": false,
+            "overview": "When a young nun at a cloistered abbey in Romania takes her own life, a priest with a haunted past and a novitiate on the threshold of her final vows are sent by the Vatican to investigate. Together they uncover the order’s unholy secret. Risking not only their lives but their faith and their very souls, they confront a malevolent force in the form of the same demonic nun that first terrorized audiences in “The Conjuring 2,” as the abbey becomes a horrific battleground between the living and the damned.",
+            "release_date": "2018-09-05"
+        },
+        {
+            "vote_count": 139,
+            "id": 347375,
+            "video": false,
+            "vote_average": 5.5,
+            "title": "Mile 22",
+            "popularity": 103.946,
+            "poster_path": "/dT1XzjxRDR56xOm4Ph0INV4EmWJ.jpg",
+            "original_language": "en",
+            "original_title": "Mile 22",
+            "genre_ids": [
+                28
+            ],
+            "backdrop_path": "/eTwftrInxzZTSLUkX5hoifOczKJ.jpg",
+            "adult": false,
+            "overview": "A CIA field officer and an Indonesian police officer are forced to work together in confronting political corruption. An informant must be moved twenty-two miles to safety.",
+            "release_date": "2018-08-16"
+        },
+        {
+            "vote_count": 214,
+            "id": 463272,
+            "video": false,
+            "vote_average": 6.2,
+            "title": "Johnny English Strikes Again",
+            "popularity": 102.101,
+            "poster_path": "/tCBxnZwLiY1BOKw3tH6AxHZdqPh.jpg",
+            "original_language": "en",
+            "original_title": "Johnny English Strikes Again",
+            "genre_ids": [
+                12,
+                10751,
+                28,
+                35
+            ],
+            "backdrop_path": "/yCOLqh5MOGyYdo58Ap0aWvKop9h.jpg",
+            "adult": false,
+            "overview": "Disaster strikes when a criminal mastermind reveals the identities of all active undercover agents in Britain. The secret service can now rely on only one man—Johnny English. Currently teaching at a minor prep school, Johnny springs back into action to find the mysterious hacker. For this mission to succeed, he’ll need all of his skills—what few he has—as the man with yesterday’s analogue methods faces off against tomorrow’s digital technology.",
+            "release_date": "2018-09-13"
+        },
+        {
+            "vote_count": 2955,
+            "id": 260513,
+            "video": false,
+            "vote_average": 7.7,
+            "title": "Incredibles 2",
+            "popularity": 75.501,
+            "poster_path": "/x1txcDXkcM65gl7w20PwYSxAYah.jpg",
+            "original_language": "en",
+            "original_title": "Incredibles 2",
+            "genre_ids": [
+                28,
+                12,
+                16,
+                10751
+            ],
+            "backdrop_path": "/mabuNsGJgRuCTuGqjFkWe1xdu19.jpg",
+            "adult": false,
+            "overview": "Elastigirl springs into action to save the day, while Mr. Incredible faces his greatest challenge yet – taking care of the problems of his three children.",
+            "release_date": "2018-06-14"
+        },
+        {
+            "vote_count": 34,
+            "id": 442062,
+            "video": false,
+            "vote_average": 5.8,
+            "title": "Goosebumps 2: Haunted Halloween",
+            "popularity": 70.443,
+            "poster_path": "/8bcpki9GfXdXj9esFpPtlate8v0.jpg",
+            "original_language": "en",
+            "original_title": "Goosebumps 2: Haunted Halloween",
+            "genre_ids": [
+                12,
+                35,
+                14,
+                27,
+                10751
+            ],
+            "backdrop_path": "/9xYg9ScjGaO2HrLfOfYZirTrB3o.jpg",
+            "adult": false,
+            "overview": "Two boys face an onslaught from witches, monsters, ghouls and a talking dummy after they discover a mysterious book by author R. L. Stine.",
+            "release_date": "2018-10-11"
+        },
+        {
+            "vote_count": 119,
+            "id": 446894,
+            "video": false,
+            "vote_average": 6.4,
+            "title": "Smallfoot",
+            "popularity": 70.281,
+            "poster_path": "/4nKoB6wMVXfsYgRZK5lHZ5VMQ6J.jpg",
+            "original_language": "en",
+            "original_title": "Smallfoot",
+            "genre_ids": [
+                35,
+                16,
+                10751,
+                12,
+                14
+            ],
+            "backdrop_path": "/7t88SoT3Dd8DhGnQuVoSbMNUl3W.jpg",
+            "adult": false,
+            "overview": "A bright young yeti finds something he thought didn't exist—a human. News of this “smallfoot” throws the simple yeti community into an uproar over what else might be out there in the big world beyond their snowy village.",
+            "release_date": "2018-09-20"
+        },
+        {
+            "vote_count": 1077,
+            "id": 447200,
+            "video": false,
+            "vote_average": 6.1,
+            "title": "Skyscraper",
+            "popularity": 67.187,
+            "poster_path": "/5LYSsOPzuP13201qSzMjNxi8FxN.jpg",
+            "original_language": "en",
+            "original_title": "Skyscraper",
+            "genre_ids": [
+                28,
+                53,
+                18
+            ],
+            "backdrop_path": "/oMKFQmoVgB69fyXfSMu0lGlHJP2.jpg",
+            "adult": false,
+            "overview": "Framed and on the run, a former FBI agent must save his family from a blazing fire in the world's tallest building.",
+            "release_date": "2018-07-11"
+        },
+        {
+            "vote_count": 236,
+            "id": 439015,
+            "video": false,
+            "vote_average": 4.4,
+            "title": "Slender Man",
+            "popularity": 60.828,
+            "poster_path": "/huSncs4RyvQDBmHjBBYHSBYJbSJ.jpg",
+            "original_language": "en",
+            "original_title": "Slender Man",
+            "genre_ids": [
+                9648,
+                53
+            ],
+            "backdrop_path": "/5K0fgMaJSdEvqSsS8e3Ez4TpvXR.jpg",
+            "adult": false,
+            "overview": "In a small town in Massachusetts, four high school girls perform a ritual in an attempt to debunk the lore of Slender Man. When one of the girls goes mysteriously missing, they begin to suspect that she is, in fact, his latest victim.",
+            "release_date": "2018-08-10"
+        },
+        {
+            "vote_count": 106,
+            "id": 446021,
+            "video": false,
+            "vote_average": 6.5,
+            "title": "Bad Times at the El Royale",
+            "popularity": 53.282,
+            "poster_path": "/iNtFgXqXPRMkm1QO8CHn5sHfUgE.jpg",
+            "original_language": "en",
+            "original_title": "Bad Times at the El Royale",
+            "genre_ids": [
+                53,
+                80,
+                9648
+            ],
+            "backdrop_path": "/gb3TVVZNNxVGNfS1NxGaiWZfwnW.jpg",
+            "adult": false,
+            "overview": "Seven strangers, each with a secret to bury, meet at Lake Tahoe's El Royale, a rundown hotel with a dark past in 1969. Over the course of one fateful night, everyone will have a last shot at redemption.",
+            "release_date": "2018-10-04"
+        },
+        {
+            "vote_count": 301,
+            "id": 399360,
+            "video": false,
+            "vote_average": 5.3,
+            "title": "Alpha",
+            "popularity": 53.26,
+            "poster_path": "/afdZAIcAQscziqVtsEoh2PwsYTW.jpg",
+            "original_language": "en",
+            "original_title": "Alpha",
+            "genre_ids": [
+                12,
+                18
+            ],
+            "backdrop_path": "/nKMeTdm72LQ756Eq20uTjF1zDXu.jpg",
+            "adult": false,
+            "overview": "After a hunting expedition goes awry, a young caveman struggles against the elements to find his way home.",
+            "release_date": "2018-08-17"
+        },
+        {
+            "vote_count": 686,
+            "id": 487558,
+            "video": false,
+            "vote_average": 7.5,
+            "title": "BlacKkKlansman",
+            "popularity": 53.05,
+            "poster_path": "/bT5WuAsjDJYQv2vXbWGDQTmjKav.jpg",
+            "original_language": "en",
+            "original_title": "BlacKkKlansman",
+            "genre_ids": [
+                80,
+                18,
+                35,
+                36
+            ],
+            "backdrop_path": "/gMVdhfQ7q9DFHhDkehrququjGPd.jpg",
+            "adult": false,
+            "overview": "Ron Stallworth, an African-American police officer from Colorado, successfully manages to infiltrate the local Ku Klux Klan and become the head of the local chapter.",
+            "release_date": "2018-08-09"
+        },
+        {
+            "vote_count": 8,
+            "id": 523873,
+            "video": false,
+            "vote_average": 4.1,
+            "title": "Kung Fu League",
+            "popularity": 51.271,
+            "poster_path": "/rW0A73hjzPWVwADlCTLnjLhAFLX.jpg",
+            "original_language": "zh",
+            "original_title": "功夫联盟",
+            "genre_ids": [
+                28,
+                35
+            ],
+            "backdrop_path": null,
+            "adult": false,
+            "overview": "Martial arts comedy following a group of kung fu legends banding together to take on the bad guys. The legends includes VINCENT ZHAO reprising his role as ‘Wong Fei Hung’ with DENNIS TO once again portraying ‘Wing Chun’ master ‘Ip Man’, DANNY CHAN KWOK KWAN as ‘Chen Zhen’ and ANDY ON as master ‘Huo Yuan Jia’.",
+            "release_date": "2018-10-19"
+        },
+        {
+            "vote_count": 700,
+            "id": 345887,
+            "video": false,
+            "vote_average": 6.2,
+            "title": "The Equalizer 2",
+            "popularity": 49.662,
+            "poster_path": "/cQvc9N6JiMVKqol3wcYrGshsIdZ.jpg",
+            "original_language": "en",
+            "original_title": "The Equalizer 2",
+            "genre_ids": [
+                53,
+                28,
+                80
+            ],
+            "backdrop_path": "/z6KLDE72SxE1A8JcJTmmnWArOvR.jpg",
+            "adult": false,
+            "overview": "Robert McCall returns to deliver his special brand of vigilante justice—but how far will he go when it’s someone he loves?",
+            "release_date": "2018-07-19"
+        },
+        {
+            "vote_count": 488,
+            "id": 445651,
+            "video": false,
+            "vote_average": 6.8,
+            "title": "The Darkest Minds",
+            "popularity": 49.076,
+            "poster_path": "/94RaS52zmsqaiAe1TG20pdbJCZr.jpg",
+            "original_language": "en",
+            "original_title": "The Darkest Minds",
+            "genre_ids": [
+                878,
+                53
+            ],
+            "backdrop_path": "/5BxrMNGl3YDiWgHCVJu8iLQoJDM.jpg",
+            "adult": false,
+            "overview": "After a disease kills 98% of America's children, the surviving 2% develop superpowers and are placed in internment camps. A 16-year-old girl escapes her camp and joins a group of other teens on the run from the government.",
+            "release_date": "2018-08-02"
+        },
+        {
+            "vote_count": 612,
+            "id": 400535,
+            "video": false,
+            "vote_average": 6.7,
+            "title": "Sicario: Day of the Soldado",
+            "popularity": 48.866,
+            "poster_path": "/msqWSQkU403cQKjQHnWLnugv7EY.jpg",
+            "original_language": "en",
+            "original_title": "Sicario: Day of the Soldado",
+            "genre_ids": [
+                28,
+                80,
+                18,
+                53
+            ],
+            "backdrop_path": "/5vlYaha6IpsTOUIPZExO7Ruvzyn.jpg",
+            "adult": false,
+            "overview": "Agent Matt Graver teams up with operative Alejandro Gillick to prevent Mexican drug cartels from smuggling terrorists across the United States border.",
+            "release_date": "2018-06-27"
+        }
+    ],
+    "page": 1,
+    "total_results": 1221,
+    "dates": {
+        "maximum": "2018-10-22",
+        "minimum": "2018-09-04"
+    },
+    "total_pages": 62
+}
