923c999b4bdacdf500d04a7d42a54b538a8bb3dc
Alexander Rau <alexanderrau86@yahoo.com>
2018-10-24 15:30:31 -0600

    Add mock fetch call cleaner test

--- src/Utils/Cleaners/Cleaners.test.js
+++ src/Utils/Cleaners/Cleaners.test.js
@@ -1,1 +1,15 @@
-import * as fetchMovies from './API';
+import * as API from '../API/';
+import { cleanMovieData } from './'
+import mockMovies from '../MockMovies';
+
+describe('Cleaners', () => {
+  API.fetchMovies = jest.fn(() => { return mockMovies })
+
+  describe('cleanMovieData', () => {
+
+    it('should call fetchMovies', async () => {
+      await cleanMovieData()
+      await expect(API.fetchMovies).toHaveBeenCalled()
+    })
+  })
+})
