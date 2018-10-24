cff24ad3bc7c35286d1bc0c2df45bdd57df3a4af
Alexander Rau <alexanderrau86@yahoo.com>
2018-10-24 15:33:28 -0600

    Write tests for cleaner

--- src/Utils/MockCleanMovies.js
+++ src/Utils/MockCleanMovies.js
@@ -2,7 +2,7 @@ export default [
   {
     backdrop_path: "http://image.tmdb.org/t/p/w1280/VuukZLgaCrho2Ar8Scl9HtV3yD.jpg",
     id: 335983,
-    overview: "When Eddie Brock acquires the powers of a symbiote, he will have to release his alter-ego "Venom" to save his life.",
+    overview: "When Eddie Brock acquires the powers of a symbiote, he will have to release his alter-ego \"Venom\" to save his life.",
     poster_path: "https://image.tmdb.org/t/p/w300_and_h450_bestv2/2uNW4WbgBXL25BAbXGLnLqX71Sw.jpg",
     release_date: "2018-10-03",
     title: "Venom",
