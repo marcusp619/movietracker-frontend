d745f26e90604225822ea1504304890a4c458877
marcusp619 <marcusp619@gmail.com>
2018-10-25 16:42:11 -0600

    Add user actions objects

--- src/Actions/user-actions.js
+++ src/Actions/user-actions.js
@@ -5,3 +5,12 @@ export const newUser = (name, email, password) => ({
   password,
 });
 
+export const fetchUser = (status) => ({
+  type: 'FETCH_USER',
+  status,
+});
+
+export const toggleUserStatus = (status) => ({
+  type: 'TOGGLE_STATUS',
+  status: !status,
+});
