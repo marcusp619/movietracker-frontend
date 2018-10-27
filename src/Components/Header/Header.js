07f49e6c099b62eb77f59649716ac11043e0e429
Dina <dcaraballo26@gmail.com>
2018-10-27 11:13:10 -0600

    Add header component to file structure and preliminary styling

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <span><i class="fas fa-sign-out-alt"></i></span>
+      <span className="app-title">
+        <span className="sparkle">MovieTracker</span>
+      </span>
+    </div>
+  )
+}
+
+export default Header;
