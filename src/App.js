229106529f4145fd7dd0c2bd6f9df23ebd79797c
marcusp619 <marcusp619@gmail.com>
2018-10-28 12:36:25 -0600

    Cleaned up code format in root folder

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import * as DataCleaner from './Utils/Cleaners/';
 import MovieContainer from './Containers/MovieContainer/MovieContainer';
 import './App.css';
-import { connect } from 'react-redux'
-import { Route, Redirect } from 'react-router-dom'
-import { withRouter } from 'react-router';
-import { addMovies } from './Actions'
+import {connect} from 'react-redux';
+import {Route, Redirect} from 'react-router-dom';
+import {withRouter} from 'react-router';
+import {addMovies} from './Actions';
 import NewUserForm from './Components/NewUserForm/NewUserForm';
 import UserLoginForm from './Containers/UserLoginForm/UserLoginForm';
 
 class App extends Component {
-
   async componentDidMount() {
     const movieInfo = await DataCleaner.cleanMovieData();
-    this.props.addMovies(movieInfo)
+    this.props.addMovies(movieInfo);
   }
 
   render() {
@@ -24,33 +23,41 @@ class App extends Component {
             <span className="sparkle">MovieTracker</span>
           </span>
         </div>
-        <Route exact path='/newuser' component={NewUserForm} />
-        <Route exact path='/' render={() => (
-          !this.props.user ? 
-            <Redirect to='/login'/> :
-            <MovieContainer />
-          )} />
-        <Route exact path='/login' render={() => (
-          this.props.user ? 
-            <Redirect to='/'/> :
-            <UserLoginForm />
-          )} />
+        <Route exact path="/newuser" component={NewUserForm} />
+        <Route
+          exact
+          path="/"
+          render={() =>
+            !this.props.user ? <Redirect to="/login" /> : <MovieContainer />
+          }
+        />
+        <Route
+          exact
+          path="/login"
+          render={() =>
+            this.props.user ? <Redirect to="/" /> : <UserLoginForm />
+          }
+        />
       </div>
     );
   }
-
 }
 
-const mapStateToProps = (state) => ({
-  user: state.user
-})
+const mapStateToProps = state => ({
+  user: state.user,
+});
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
-    addMovies: (movieInfo) => {
-      dispatch(addMovies(movieInfo))
-    }
-  }
-}
+    addMovies: movieInfo => {
+      dispatch(addMovies(movieInfo));
+    },
+  };
+};
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps,
+  )(App),
+);
