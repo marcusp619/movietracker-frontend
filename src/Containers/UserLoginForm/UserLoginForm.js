15d36da58230caa4e20eff675212ced17e572889
Dina <dcaraballo26@gmail.com>
2018-10-28 13:11:06 -0600

    Adjust spacing on login form

--- src/Containers/UserLoginForm/UserLoginForm.js
+++ src/Containers/UserLoginForm/UserLoginForm.js
@@ -27,24 +27,26 @@ class UserLoginForm extends Component {
   render() {
     const {email, value} = this.state;
     return (
-      <form className="user-login-form" onSubmit={this.handleSubmit}>
-        <input
-          className="form-input user-email-login"
-          name="email"
-          value={email}
-          onChange={this.handleChange}
-          placeholder="Enter Email Address"
-        />
-        <input
-          className="form-input user-password-login"
-          type="password"
-          name="password"
-          value={value}
-          onChange={this.handleChange}
-          placeholder="Enter Password"
-        />
-        <button className="login-button">Go To Movies!</button>
-      </form>
+      <div className="form-wrapper">
+        <form className="user-login-form" onSubmit={this.handleSubmit}>
+          <input
+            className="form-input user-email-login"
+            name="email"
+            value={email}
+            onChange={this.handleChange}
+            placeholder="Enter Email Address"
+          />
+          <input
+            className="form-input user-password-login"
+            type="password"
+            name="password"
+            value={value}
+            onChange={this.handleChange}
+            placeholder="Enter Password"
+          />
+          <button className="login-button">Go To Movies!</button>
+        </form>
+      </div>
     );
   }
 }
