3ae5539e6542e8c53ee0dc9dd07cfdf2b0acbcf6
Dina <dcaraballo26@gmail.com>
2018-10-27 23:11:43 -0600

    Change font on movie card to complement app title

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import './Header.css';
 
-const Header = () => {
+const Header = (props) => {
   return (
     <div className="header">
-      <span><i className="fas fa-sign-out-alt"></i></span>
+      <span>
+        <i className="fas fa-sign-out-alt"></i>
+      </span>
       <span className="app-title">
         <span className="sparkle">MovieTracker</span>
       </span>
     </div>
   )
 }
+//signOutUser action creator function must be called
+//need onClick function that dispatches the signout action object
+//action object applied by users reducer
+//reducer updates global state object with blank string value for the user key
+
 
 export default Header;
