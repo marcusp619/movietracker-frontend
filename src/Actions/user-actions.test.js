625b4fb28534d14fa9577abd6f10922b0b53babb
Dina <dcaraballo26@gmail.com>
2018-10-28 16:00:04 -0600

    Add passing test for sign in user action creator

--- src/Actions/user-actions.test.js
+++ src/Actions/user-actions.test.js
@@ -4,7 +4,6 @@ describe('User Actions Creators', () => {
 
   describe('newUser action creator', () => {
     it('should have a type of NEW_USER', () => {
-      //name, email, password
       const mockName = 'Tupac Shakur'
       const mockEmail = 'tupac@thuglife.com'
       const mockPassword = 'onlyGodCanJudgeMe'
@@ -23,7 +22,21 @@ describe('User Actions Creators', () => {
 
   describe('signInUser action creator', () => {
     it('should have a type of SIGN_IN_USER', () => {
+      const mockUser = {
+        name: 'Tupac Shakur',
+        id: 1,
+        email: 'tupac@thuglife.com',
+      }
+      const expectedAction = {
+        type: 'SIGN_IN_USER',
+        id: 1,
+        name: 'Tupac Shakur',
+        email: 'tupac@thuglife.com',
+      }
 
+      const result = UserActions.signInUser(mockUser)
+
+      expect(result).toEqual(expectedAction)
     })
   })
 
