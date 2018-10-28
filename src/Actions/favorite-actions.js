519f4762f998d6f78153a550bf0d6cfd868c27d6
Alexander Rau <alexanderrau86@yahoo.com>
2018-10-28 17:58:06 -0600

    Clicking favorites button twice removes the favorite

--- src/Actions/favorite-actions.js
+++ src/Actions/favorite-actions.js
@@ -8,3 +8,9 @@ export const addFavorite = (movie, user) => ({
   vote_average: movie.vote_average,
   overview: movie.overview,
 });
+
+export const removeFavorite = (movie, user) => ({
+  type: 'DELETE_FAVORITE',
+  user_id: user.id,
+  movie_id: movie.id
+});
