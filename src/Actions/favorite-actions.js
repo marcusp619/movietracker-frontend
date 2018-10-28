a743f41a89358bdbba6e24ec7d80629c84a4624c
marcusp619 <marcusp619@gmail.com>
2018-10-28 12:24:14 -0600

    Cleaned up style format in actions folder

--- src/Actions/favorite-actions.js
+++ src/Actions/favorite-actions.js
@@ -6,5 +6,5 @@ export const addFavorite = (movie, user) => ({
   poster_path: movie.poster_path,
   release_date: movie.release_date,
   vote_average: movie.vote_average,
-  overview: movie.overview
+  overview: movie.overview,
 });
