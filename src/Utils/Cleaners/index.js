ea1306c295fa465ef3b25a1f1e87af6cf18264b3
marcusp619 <marcusp619@gmail.com>
2018-10-28 12:35:29 -0600

    Cleaned up code format in utils folder

--- src/Utils/Cleaners/index.js
+++ src/Utils/Cleaners/index.js
@@ -1,23 +1,29 @@
 import * as API from '../API/';
 
 export const cleanMovieData = async () => {
-  const rawMovieData = await API.fetchMovies()
+  const rawMovieData = await API.fetchMovies();
   const posterURL = 'https://image.tmdb.org/t/p/w300_and_h450_bestv2';
   const backdropURL = 'http://image.tmdb.org/t/p/w1280';
   return rawMovieData.results.map(movie => {
-    const {title, id, poster_path, backdrop_path, release_date, vote_average, overview} = movie;
-    
-    return {
-      title, 
-      id, 
-      poster_path: `${posterURL}${poster_path}`, 
-      backdrop_path: `${backdropURL}${backdrop_path}`,
-      release_date, 
-      vote_average, 
+    const {
+      title,
+      id,
+      poster_path,
+      backdrop_path,
+      release_date,
+      vote_average,
       overview,
-      favorite: false
-    }
+    } = movie;
+
+    return {
+      title,
+      id,
+      poster_path: `${posterURL}${poster_path}`,
+      backdrop_path: `${backdropURL}${backdrop_path}`,
+      release_date,
+      vote_average,
+      overview,
+      favorite: false,
+    };
   });
-}
-
-
+};
