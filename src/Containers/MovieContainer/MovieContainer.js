8f12ee30d6ac962bf94ede901aaae2586a205417
marcusp619 <marcusp619@gmail.com>
2018-10-28 12:30:21 -0600

    Cleaned up code format in containers folder

--- src/Containers/MovieContainer/MovieContainer.js
+++ src/Containers/MovieContainer/MovieContainer.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import './MovieContainer.css';
 import MovieCard from '../../Components/MovieCard/MovieCard';
 
 const MovieContainer = ({movies}) => {
-  
-  const cards = movies.map((movie) => {
-    return <MovieCard key={movie.id} {...movie} />
-  })
+  const cards = movies.map(movie => {
+    return <MovieCard key={movie.id} {...movie} />;
+  });
 
-  return (
-    <div className='movie-container'>
-      { cards }
-    </div>
-  )
-}
+  return <div className="movie-container">{cards}</div>;
+};
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
-    movies: state.movies
-  }
-}
+    movies: state.movies,
+  };
+};
 
 export default connect(mapStateToProps)(MovieContainer);
--- src/Containers/UserLoginForm/UserLoginForm.js
+++ src/Containers/UserLoginForm/UserLoginForm.js
@@ -1,62 +1,65 @@
-import React, { Component } from 'react';
-import { signInUser } from '../../Actions/user-actions';
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import {signInUser} from '../../Actions/user-actions';
+import {connect} from 'react-redux';
 import * as API from '../../Utils/API/';
-import './UserLoginForm.css'
+import './UserLoginForm.css';
 
 class UserLoginForm extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       email: '',
-      password: ''
-    }
+      password: '',
+    };
   }
 
-  handleChange = (e) => {
-    const { name, value } = e.target
-    this.setState({ [name]: value })
-  }
+  handleChange = e => {
+    const {name, value} = e.target;
+    this.setState({[name]: value});
+  };
 
-  handleSubmit = async (e) => {
-    e.preventDefault()
-      const fetchedUser = await API.checkUser(this.state)
-      this.props.userSignIn(fetchedUser) 
-  }
+  handleSubmit = async e => {
+    e.preventDefault();
+    const fetchedUser = await API.checkUser(this.state);
+    this.props.userSignIn(fetchedUser);
+  };
 
   render() {
-    const { email, value } = this.state
+    const {email, value} = this.state;
     return (
-      <form className='user-login-form' onSubmit={this.handleSubmit}>
+      <form className="user-login-form" onSubmit={this.handleSubmit}>
         <input
-          className="form-input user-email-login" 
+          className="form-input user-email-login"
           name="email"
           value={email}
           onChange={this.handleChange}
-          placeholder="Enter Email Address" />
-        <input 
+          placeholder="Enter Email Address"
+        />
+        <input
           className="form-input user-password-login"
           type="password"
           name="password"
           value={value}
           onChange={this.handleChange}
-          placeholder="Enter Password" />
-        <button className='login-button'>
-          Go To Movies!
-        </button>
+          placeholder="Enter Password"
+        />
+        <button className="login-button">Go To Movies!</button>
       </form>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => ({
-  user: state.user
-})
+const mapStateToProps = state => ({
+  user: state.user,
+});
 
-const mapDispatchToProps = (dispatch) => ({
-  userSignIn: (user) => {
-    dispatch(signInUser(user))
-  }
-})
+const mapDispatchToProps = dispatch => ({
+  userSignIn: user => {
+    dispatch(signInUser(user));
+  },
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(UserLoginForm)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(UserLoginForm);
