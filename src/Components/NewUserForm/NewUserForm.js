5cbaecbd750e05843b3880528b1aa9aab7a737d5
Dina <dcaraballo26@gmail.com>
2018-10-28 20:43:15 -0600

    Add styling to NewUserForm

--- src/Components/NewUserForm/NewUserForm.js
+++ src/Components/NewUserForm/NewUserForm.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import * as API from '../../Utils/API';
+import './NewUserForm.css';
 
 class NewUserForm extends Component {
   constructor(props) {
@@ -25,31 +26,33 @@ class NewUserForm extends Component {
   render() {
     const {name, email, password} = this.state;
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input
-          name="name"
-          className="user-name-input"
-          value={name}
-          onChange={this.handleChange}
-          placeholder="Enter Full Name"
-        />
-        <input
-          name="email"
-          className="user-email-input"
-          value={email}
-          onChange={this.handleChange}
-          placeholder="Enter Email Address"
-        />
-        <input
-          name="password"
-          type="password"
-          className="user-password-input"
-          value={password}
-          onChange={this.handleChange}
-          placeholder="Enter Password"
-        />
-        <button>Submit</button>
-      </form>
+      <div className="form-wrapper">
+        <form className="new-user-form" onSubmit={this.handleSubmit}>
+          <input
+            name="name"
+            className="form-input user-name-input"
+            value={name}
+            onChange={this.handleChange}
+            placeholder="Enter Full Name"
+          />
+          <input
+            name="email"
+            className="form-input user-email-input"
+            value={email}
+            onChange={this.handleChange}
+            placeholder="Enter Email Address"
+          />
+          <input
+            name="password"
+            type="password"
+            className="form-input user-password-input"
+            value={password}
+            onChange={this.handleChange}
+            placeholder="Enter Password"
+          />
+          <button className="signup-button">Sign Up!</button>
+        </form>
+      </div>
     );
   }
 }
