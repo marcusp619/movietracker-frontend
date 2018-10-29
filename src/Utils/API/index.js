781047c5302d869e473bac380bca350cb1659bcc
marcusp619 <marcusp619@gmail.com>
2018-10-29 08:01:39 -0600

    updated checkuser fetch

--- src/Utils/API/index.js
+++ src/Utils/API/index.js
@@ -1,4 +1,4 @@
-import {key} from './API_KEY';
+import { key } from "./API_KEY";
 
 export const fetchMovies = async () => {
   const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=1`;
@@ -8,46 +8,43 @@ export const fetchMovies = async () => {
 };
 
 export const postNewUser = async newUserInfo => {
-  const response = await fetch('http://localhost:3000/api/users/new', {
-  method: 'POST',
-  body: JSON.stringify({...newUserInfo}),
-  headers: {'Content-Type': 'application/json'},
-  });  
+  const response = await fetch("http://localhost:3000/api/users/new", {
+    method: "POST",
+    body: JSON.stringify({ ...newUserInfo }),
+    headers: { "Content-Type": "application/json" }
+  });
   const result = await response.json();
   return result;
-  //  .then(res => res.json())
-  //  .then(response => console.log('Success:', JSON.stringify(response)))
-  //  .catch(error => console.error('Error:', error));
 };
 
 export const checkUser = async userInfo => {
-  console.log(userInfo);
-  const response = await fetch('http://localhost:3000/api/users', {
-    method: 'POST',
+  const response = await fetch("http://localhost:3000/api/users", {
+    method: "POST",
     body: JSON.stringify(userInfo),
-    headers: {'Content-Type': 'application/json'},
+    headers: { "Content-Type": "application/json" }
   });
-  const result = await response.json();
-  const {name, email, id} = result.data;
-  return {name, email, id};
+  const result = await response.text();
+  console.log(result);
+  const { name, email, id } = result.data;
+  return { name, email, id };
 };
 
 export const addFav = async favInfo => {
   const response = await fetch(
-    'http://localhost:3000/api/users/favorites/new',
+    "http://localhost:3000/api/users/favorites/new",
     {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify(favInfo),
-      headers: {'Content-Type': 'application/json'},
-    },
+      headers: { "Content-Type": "application/json" }
+    }
   );
   const result = await response.json();
-  console.log('Success:', JSON.stringify(result));
+  console.log("Success:", JSON.stringify(result));
 };
 
 export const getFavorites = async userId => {
   const response = await fetch(
-    `http://localhost:3000/api/users/${userId}/favorites`,
+    `http://localhost:3000/api/users/${userId}/favorites`
   );
   const result = await response.json();
   return result.data;
