4fcf05f81515e03fd5d767857c114ed110f8b10d
marcusp619 <marcusp619@gmail.com>
2018-10-30 10:22:04 -0600

    Updated style to login form

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
 
-const Header = (props) => {
+const Header = props => {
   return (
     <div className="header">
       <span>
-        <i className="fas fa-sign-out-alt"></i>
+        <i className="fas fa-sign-out-alt" />
       </span>
       <span className="app-title">
         <span className="sparkle">MovieTracker</span>
       </span>
     </div>
 
-//signOutUser action creator function must be called
-//need onClick function that dispatches the signout action object
-//action object applied by users reducer
-//reducer updates global state object with blank string value for the user key
-
+    //signOutUser action creator function must be called
+    //need onClick function that dispatches the signout action object
+    //action object applied by users reducer
+    //reducer updates global state object with blank string value for the user key
   );
 };
-
 
 export default Header;
