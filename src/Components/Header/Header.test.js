bc544845c10ed248e5717b67c30ee49647ef6296
Dina <dcaraballo26@gmail.com>
2018-10-30 19:39:18 -0600

    Add passing test for mapStateToProps in Header

--- src/Components/Header/Header.test.js
+++ src/Components/Header/Header.test.js
@@ -1,14 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './Header';
+import { Header, mapStateToProps, mapDispatchToProps } from './Header';
 import { shallow } from 'enzyme';
 
 describe('Header Component', () => {
   let wrapper;
+  const mockUser = {
+    name: 'Louisa',
+    id: 1806,
+    email: 'Louisa@turing.io',
+    password: 'oooooooooooookay'
+  };
+  const mockSignOutUser = jest.fn();
 
   it('should match the snapshot', () => {
-    wrapper = shallow(<Header />)
+    wrapper = shallow(
+      <Header
+        user={mockUser}
+        signOutUser={mockSignOutUser}
+      />
+    )
     expect(wrapper).toMatchSnapshot()
+  })
+
+  describe('mapStateToProps function', () => {
+    it('should return an object with a user', () => {
+      const mockState = {
+        movies: [
+          {title: 'Coco', vote_average: 7.6, favorite: true},
+          {title: 'Inception', vote_average: 8.6, favorite: true},
+          {title: 'Hunt for Red October', vote_average: 4.6, favorite: true}
+        ],
+        user: {name: 'Louisa', email: 'blah@gmail.com'},
+        favorites: []
+      }
+      const expected = {user: {name: 'Louisa', email: 'blah@gmail.com'}}
+      const mappedProps = mapStateToProps(mockState)
+      expect(mappedProps).toEqual(expected)
+    })
+  })
+
+  describe('mapDispatchToProps function', () => {
+
   })
 
 })
